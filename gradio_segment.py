d02c990da5e91bc22083d8be8821d7f9f12062c6
zhangchenxu <chenxu_zh01@126.com>
2026-05-18 23:10:40 +0800

    *

--- gradio_segment.py
+++ gradio_segment.py
@@ -386,4 +386,8 @@ def build_app() -> gr.Blocks:
 
 
 if __name__ == "__main__":
-    build_app().launch(server_name="127.0.0.1", server_port=7861)
+    build_app().launch(
+        server_name="0.0.0.0",
+        server_port=7860,
+        root_path="/proxy/7860",
+    )
